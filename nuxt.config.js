78ab6b69a42a59695a55549663bc78c94d80ad17
Daniel James <danielrjames@live.com>
2022-03-09 15:46:38 -0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+export default {
+  head: {
+    title: "tailwind-test",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: ["@/assets/css/main.css"],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: ["@nuxt/postcss8"],
+
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+    extractCSS: true, // turn off to see tailwind shadows and rings
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div
+      class="relative mb-10 z-50 w-full h-14 sm:h-16 bg-white ring-1 ring-gray-900 ring-opacity-5 shadow-sm"
+    >
+      Header with a shadow
+    </div>
+    <div class="max-w-lg mx-auto">
+      <button
+        class="py-3 px-6 mb-5 border border-transparent rounded shadow text-sm font-medium transition-all cursor-pointer text-white bg-blue-500 ring-2 ring-opacity-50 ring-offset-2 ring-blue-500"
+      >
+        Ring Button
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
